b8c8e0b14f6204e55dea81cfba4e5b7b190c4549
VictorOgolla <victorogola@gmail.com>
2019-09-27 20:12:03 +0300

    Learnt about conditional if statements

---
+++ lesson_1.php
@@ -0,0 +1,23 @@
+<?php
+//Php conditional statement
+//if...
+//if...else...
+//if...elseif...else
+//switch
+//if(condition){
+//   code to execute
+//}
+$time = date("H");
+echo $time;
+if($time <"20"){
+    echo "Goodday<br>";
+}
+$x = 3;
+$y = 2;
+if($x >= $y){
+    echo "variable x is greater than or equal to variable y";
+}else{
+    echo "variable x is not greater than or equal to variable y";
+    }
+
+?>
